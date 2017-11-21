6198f9c3c4efa494d67aaddb4f467540e7cd8f80
Onur Eker <yonureker@gmail.com>
2017-11-21 00:19:30 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -21,4 +21,3 @@ BoardMember.prototype.releasePressStatement = function(){
 BoardMember.prototype.sayHi = function(){
      return `Hi, my name is ${this.name}. I am from ${this.homeState}, and I was trained in ${this.training}.`
    }
- }
